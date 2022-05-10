6d60f09b66b643085319d99f28642461e71dc42e
Meghnameghu <105042430+Meghnameghu@users.noreply.github.com>
2022-05-10 22:52:55 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -5,19 +5,19 @@
 //Stats Statistics::Computedstatistics(const std::vector<___>&
 template<typename T> Statistics::Stats<T> Statistics::ComputeStatistics(const std::vector<T>& data)
  
-    Stats<T> std;
+    Stats<T> st;
      if(data.size()==0)
     {
        std.Average=NAN;
-       sdt.Max=NAN;
-       std.Min=NAN;
+       st.Max=NAN;
+       st.Min=NAN;
     }
     
-    std.Max=*max_element(data.begin(),data.end());
-    std.Min=*min_element(data.begin(),data.end());
+    st.Max=*max_element(data.begin(),data.end());
+    st.Min=*min_element(data.begin(),data.end());
     T sum=accumulate(data.begin(), data.end() , 0);
-    std.Average=(sum/data.size());
-    return std;
+    st.Average=(sum/data.size());
+    return st;
   }
     
 
